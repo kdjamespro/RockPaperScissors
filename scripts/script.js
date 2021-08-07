4893ba513ed2bc9cebea08b4281481c1d9eebb34
Kenneth Dwight James L. Probadora <jamesprobadora@gmail.com>
2021-08-07 20:25:15 +0800

    Add player character animation

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,7 @@
 const moves = ['Rock', 'Paper', 'Scissors'];
 let playerLives = 5;
 let computerLives = 5;
+let playerState = 'idle';
 
 // Initialize the DOM object we need to access
 const buttons = document.querySelectorAll('.rps-choice');
@@ -10,7 +11,15 @@ const computerScore = document.querySelector('.computerScore');
 const narration = document.querySelector('.narration');
 const menu = document.querySelector('.rps-menu');
 const hp = document.querySelector('.results');
+const game = document.querySelector('.display');
 const start = document.querySelector('.start');
+const hcanvas = document.querySelector('.human-player');
+const hctx = hcanvas.getContext('2d');
+const CANVAS_HEIGHT = hcanvas.height = 550;
+const CANVAS_WIDTH = hcanvas.width = 550;
+
+const playerImage = new Image();
+playerImage.src = 'images/human.png';
 
 
 // Returns a random move of a computer from the set of possible moves
@@ -34,12 +43,14 @@ function playRound(playerSelection, computerSelection)
         {   
             updateLives('computer', computerLives);
             computerLives -= 1;
+            playerState = 'attack';
             return 'You Win! Rock beats Scissors';
         }
         else if(computerSelection === 'paper')
         {
             updateLives('player', playerLives);
             playerLives -= 1;
+            playerState = 'hurt';
             return 'You Lose! Paper beats Rock';
         }
         else
@@ -53,12 +64,14 @@ function playRound(playerSelection, computerSelection)
         {
             updateLives('computer', computerLives);
             computerLives -= 1;
+            playerState = 'attack';
             return 'You Win! Scissors beats Paper';
         }
         else if(computerSelection === 'rock')
         {
             updateLives('player', playerLives);
             playerLives -= 1;
+            playerState = 'hurt';
             return 'You Lose! Rock beats Scissors';
         }
         else
@@ -72,12 +85,14 @@ function playRound(playerSelection, computerSelection)
         {
             updateLives('computer', computerLives);
             computerLives -= 1;
+            playerState = 'attack';
             return 'You Win! Paper beats Rock';
         }
         else if(computerSelection === 'scissors')
         {
             updateLives('player', playerLives);
             playerLives -= 1;
+            playerState = 'hurt';
             return 'You Lose! Scissors beats Paper';
         }
         else (computerSelection === 'paper')
@@ -87,6 +102,60 @@ function playRound(playerSelection, computerSelection)
     }
 
     return "You have selected Invalid Move!"
+}
+
+const spriteWidth = 550;
+const spriteHeight = 550;
+let gameFrame = 0;
+const delayFrames = 10;
+const humanAnimations = [];
+const humanStates = [
+    {
+        name: 'idle',
+        frames: 10,
+    },
+    {
+        name: 'attack',
+        frames: 10,
+    },
+    {
+        name: 'hurt',
+        frames: 10,
+    }
+];
+
+humanStates.forEach((state, index) => {
+    let frames = {
+        loc: [],
+    }
+    for(let j = 0; j <state.frames; j++){
+        let postionX = j * spriteWidth;
+        let positionY = index * spriteHeight;
+        frames.loc.push({x: postionX, y: positionY});
+    }
+    humanAnimations[state.name] = frames;
+});
+console.log(humanAnimations);
+
+function animate()
+{
+    hctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    let numFrames = humanAnimations[playerState].loc.length;
+    let position = Math.floor(gameFrame/delayFrames) % numFrames;
+    let frameX = spriteWidth * position
+    let frameY = humanAnimations[playerState].loc[position].y;
+    hctx.drawImage(playerImage, frameX, frameY,
+        spriteWidth, spriteHeight, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    
+    gameFrame += 1;
+    if(playerState == 'attack' || playerState == 'hurt')
+    {
+        if((position + 1) % numFrames == 0)
+        {
+            playerState = 'idle';
+        }
+    }
+    requestAnimationFrame(animate);
 }
 
 // Starts the game 
@@ -131,9 +200,10 @@ function resetLives()
 
 function setup()
 {
-    hp.style.display = 'block';
-    menu.style.display = "flex";
+    game.style.display = 'block';
     start.style.display = "none";
+
+    animate('idle');
     gameStart();
 }
 
